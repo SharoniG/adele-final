1a4d39d7842a0d0762c91c798db3cb82789fbccc
Shar G <sharongolani@hotmail.com>
2025-05-25 18:52:12 +0300

    configuration app.js

---
+++ server/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+const app = express();
+
+mongoose
+.connect(`mongodb+srv://argusapocraphex30:Guchtum8A5!@cluster0.uzzhosq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
+}); 
+
+
+app.use(express.json());   
+
+app.listen(3000, () => {   
+  console.log('Server is running on port 3000');
+});
