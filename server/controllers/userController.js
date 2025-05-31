a657fc09ce033fd8ab733ad720296c973c10b8b8
Shar G <sharongolani@hotmail.com>
2025-05-31 08:09:23 +0300

    product model

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -56,3 +56,5 @@ const registerUser = async (req, res) => {
    }
 
 };
+
+
---
+++ server/models/productModel.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {  
+        type: String, 
+        required: true 
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+    description: String,
+    category: {
+        type: String,
+        enum: ['general', 'stuffed', 'fashion','trend'],
+        default: 'general',
+    },
+    imageUrl: String,
+    stock: {
+      type: Number,
+      default: 0
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  });
+
+
+
+  const Product = mongoose.model('Product', userSchema);
+  
+  export default Product;    
